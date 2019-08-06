f2132219d608716a869c83009ba5ab21f2f4b203
Eugene Paitoo <eugenepaitoo@gmail.com>
2019-08-06 16:56:23 +0000

    add build for production

---
+++ build/precache-manifest.4d121cb08135cd3aa1c4dd0bd7c8806e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad6587a484d537be946fac5ecb723c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ff27b8989f2a7ef3779",
+    "url": "/static/css/main.6d467092.chunk.css"
+  },
+  {
+    "revision": "c4c3a86b69c06e9a5e62",
+    "url": "/static/js/2.ce943ffb.chunk.js"
+  },
+  {
+    "revision": "1ff27b8989f2a7ef3779",
+    "url": "/static/js/main.155ba2ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
